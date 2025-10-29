5b6a09ccd004f9ab55c0ad80ba9a2de7daf05b78
Mayank Mahajan <mayankmahajan9056@gmail.com>
2025-10-29 21:36:14 +0530

    feat: integrate MongoDB connection and add middleware dependencies
    - Updated index.js to include MongoDB connection using connectDB.
    - Changed server start logic to listen on the specified PORT from environment variables.
    - Added cookie-parser and cors as dependencies in package.json and package-lock.json.
    - Included mongoose for MongoDB object modeling.
    - Added morgan for logging HTTP requests and winston for logging management in devDependencies.

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,65 @@
+import cookieParser from "cookie-parser";
 import express from "express"
+import morgan from "morgan";
+import cors from "cors"
+import 'dotenv/config'
 
 const app = express()
 
-app.get("/",(req,res) => {
-   res.send("Hello from express")
-})
+// logger setup
+if (process.env.NODE_ENV === "development") {
+  const morganFormat = ":method :url :status :response-time ms";
+
+  app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
+}
+
+  // Body parser middleware
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extends: true, limit: "16kb" }));
+app.use(cookieParser());
+
+// CORS configuration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "device-remember-token",
+      "Access-Control-Allow-Origin",
+      "Origin",
+      "Access",
+    ],
+  })
+);
+
+// Api routes imports
+
+
+// Api routes
+
+app.use((req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: "Route not found ❌",
+  });
+});
 
 export {app}
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 import { app } from "./app.js";
+import connectDB from "./db/index.js";
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+connectDB().then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
