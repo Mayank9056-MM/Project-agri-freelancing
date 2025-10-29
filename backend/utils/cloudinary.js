3357c8d9c51cb53441ab918783c74b8acbc78938
Mayank Mahajan <mayankmahajan9056@gmail.com>
2025-10-30 00:27:59 +0530

    feat: add Cloudinary utility for file uploads and configuration

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// configure cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("File upload on cloudinary. File src: ", response.url);
+    // once the file is uploaded, we would like to delete it from our server
+    if (fs.existsSync(localFilePath)) {
+      fs.unlinkSync(localFilePath);
+    }
+    return response;
+  } catch (error) {
+    if (fs.existsSync(localFilePath)) {
+      fs.unlinkSync(localFilePath);
+    }
+    return null;
+  }
+};
