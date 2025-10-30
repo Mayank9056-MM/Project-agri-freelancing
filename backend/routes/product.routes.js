27c02a95f72cfe67efc747c0d8fe730821ce8537
Mayank Mahajan <mayankmahajan9056@gmail.com>
2025-10-30 16:33:40 +0530

    feat: add getAllProducts route to product management

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { verifyAdmin, verifyAuth } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.js";
-import { createProduct, deleteProduct, updateProduct } from "../controllers/product.controllers.js";
+import { createProduct, deleteProduct, getAllProducts, updateProduct } from "../controllers/product.controllers.js";
 
 const productRouter = express.Router()
 
-productRouter.route("/").post(verifyAuth,verifyAdmin,upload.single("image"),createProduct)
+productRouter.route("/").post(verifyAuth,verifyAdmin,upload.single("image"),createProduct).get(verifyAuth,verifyAdmin,getAllProducts)
 productRouter.route("/:sku").patch(verifyAuth,verifyAdmin,upload.single("image"),updateProduct).delete(verifyAuth,verifyAdmin,deleteProduct)
 
 export default productRouter;
