58e3b2708079eab20ab8cc4e50e2893cf6e92fc3
Mayank Mahajan <mayankmahajan9056@gmail.com>
2025-11-04 02:12:17 +0530

    add  more units constant for product model

--- backend/constants.js
+++ backend/constants.js
@@ -1,1 +1,20 @@
 export const DB_NAME = "stock-pricing";
+
+export const PRODUCT_UNITS = [
+  "pcs", // pieces
+  "box",
+  "pack",
+  "packet",
+  "kg",
+  "g",
+  "mg",
+  "litre",
+  "ml",
+  "dozen",
+  "meter",
+  "cm",
+  "inch",
+  "set",
+  "pair",
+  "bundle",
+];
