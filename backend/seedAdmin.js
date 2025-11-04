6472dc9cc4a92fbe1b22f0d79337db7df2640e81
Mayank Mahajan <mayankmahajan9056@gmail.com>
2025-11-04 17:09:33 +0530

    bug fix

--- backend/seedAdmin.js
+++ backend/seedAdmin.js
@@ -2,11 +2,43 @@ import mongoose from "mongoose";
 import User from "./models/User.model.js";
 import dotenv from "dotenv";
 import { DB_NAME } from "./constants.js";
-import { uploadOnCloudinary } from "./utils/cloudinary.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
 dotenv.config();
 
-const createAdmin = async (req, res) => {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// configure cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    console.log("Cloudinary config:", cloudinary.config());
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+      folder: "adminAvatar",
+    });
+
+    console.log("File upload on cloudinary. File src: ", response.url);
+    return response;
+  } catch (error) {
+    console.error("❌ Cloudinary upload error:", error.message);
+    return null;
+  }
+};
+
+const createAdmin = async () => {
   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
 
   let admin = await User.findOne({ role: "admin" });
@@ -14,22 +46,22 @@ const createAdmin = async (req, res) => {
     console.log("Creating admin role...");
     let avatar;
     try {
-       avatar = uploadOnCloudinary("/public/admin.svg");
+     const imagePath = path.join(__dirname, "public", "admin.png");
+      console.log("Resolved image path:", imagePath);
+      console.log("File exists?", fs.existsSync(imagePath));
+      avatar = await uploadOnCloudinary(imagePath);
+
+      console.log(avatar);
 
       if (!avatar) {
-        return res
-          .status(400)
-          .json({
-            message: "somethng went wrong while uploading image for admin",
-          });
+        console.error("❌ Failed to upload avatar");
+        await mongoose.connection.close();
+        return process.exit(1);
       }
     } catch (error) {
       console.log(error);
-      res
-        .status(400)
-        .json({
-          message: "somethng went wrong while uploading image for admin",
-        });
+
+      throw new Error("Failed to upload avatar", avatar);
     }
 
     admin = await User.create({
