8cbdb5a173cb84d53fa6353ed56c82784689ab63
Mayank Mahajan <mayankmahajan9056@gmail.com>
2025-11-04 16:19:10 +0530

    add image for admin

--- backend/seedAdmin.js
+++ backend/seedAdmin.js
@@ -2,20 +2,42 @@ import mongoose from "mongoose";
 import User from "./models/User.model.js";
 import dotenv from "dotenv";
 import { DB_NAME } from "./constants.js";
+import { uploadOnCloudinary } from "./utils/cloudinary.js";
 
 dotenv.config();
 
-const createAdmin = async () => {
+const createAdmin = async (req, res) => {
   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
 
   let admin = await User.findOne({ role: "admin" });
   if (!admin) {
     console.log("Creating admin role...");
+
+    try {
+      const avatar = uploadOnCloudinary("/public/admin.svg");
+
+      if (!avatar) {
+        return res
+          .status(400)
+          .json({
+            message: "somethng went wrong while uploading image for admin",
+          });
+      }
+    } catch (error) {
+      console.log(error);
+      res
+        .status(400)
+        .json({
+          message: "somethng went wrong while uploading image for admin",
+        });
+    }
+
     admin = await User.create({
       fullName: "owner",
       email: "admin@gmail.com",
       role: "admin",
       password: "123456789",
+      avatar: avatar?.secure_url,
     });
 
     mongoose.connection.close();
